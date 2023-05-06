0117b6db807b5b926ed72e7c74e474dc6bc02f42
qu4lizz <miroslavbratic00@gmail.com>
2023-05-06 13:36:13 +0200

    feat: compiler optimizations

--- build.sh
+++ build.sh
@@ -11,3 +11,27 @@ g++ -fopenmp src/huffman_compression_openmp.cpp -o huffman_compression_openmp.ex
 
 echo "Compiling hufman_compression_cuda_omp.cu"
 nvcc src/huffman_compression_cuda_omp.cu -o huffman_compression_cuda_omp.exe -Xcompiler -fopenmp
+
+echo "Compiling huffman_compression_o2.cpp"
+g++ -O2 src/huffman_compression.cpp -o huffman_compression.exe
+
+echo "Compiling huffman_compression_o3.cpp"
+g++ -O3 src/huffman_compression.cpp -o huffman_compression.exe
+
+echo "Compiling huffman_compression_cuda_o2.cu"
+nvcc -O2 src/huffman_compression_cuda.cu -o huffman_compression_cuda.exe -I/opt/cuda/include -L/opt/cuda/lib64 -lcudart
+
+echo "Compiling huffman_compression_cuda_o3.cu"
+nvcc -O3 src/huffman_compression_cuda.cu -o huffman_compression_cuda.exe -I/opt/cuda/include -L/opt/cuda/lib64 -lcudart
+
+echo "Compiling huffman_compression_openmp_o2.cpp"
+g++ -O2 -fopenmp src/huffman_compression_openmp.cpp -o huffman_compression_openmp.exe
+
+echo "Compiling huffman_compression_openmp_o3.cpp"
+g++ -O3 -fopenmp src/huffman_compression_openmp.cpp -o huffman_compression_openmp.exe
+
+echo "Compiling hufman_compression_cuda_omp_o2.cu"
+nvcc -O2 src/huffman_compression_cuda_omp.cu -o huffman_compression_cuda_omp.exe -Xcompiler -fopenmp
+
+echo "Compiling hufman_compression_cuda_omp_o3.cu"
+nvcc -O3 src/huffman_compression_cuda_omp.cu -o huffman_compression_cuda_omp.exe -Xcompiler -fopenmp
--- run.sh
+++ run.sh
@@ -6,20 +6,40 @@ output_file="$2"
 
 # Call huffman_compression with input and output filenames as arguments
 ./huffman_compression.exe "$input_file" "$output_file.out"
-echo "Compression: $(echo "scale=2; $(stat -c '%s' $output_file.out) / $(stat -c '%s' $input_file)" | bc -l)%"
+echo ''
+
+./huffman_compression_o2.exe "$input_file" "$output_file.out"
+echo ''
+
+./huffman_compression_o3.exe "$input_file" "$output_file.out"
 echo ''
 
 # Call huffman_compression_cuda with input and output filenames as arguments
 ./huffman_compression_cuda.exe "$input_file" "$output_file.cuda"
-echo "Compression: $(echo "scale=2; $(stat -c '%s' $output_file.cuda) / $(stat -c '%s' $input_file)" | bc -l)%"
+echo ''
+
+./huffman_compression_cuda_o2.exe "$input_file" "$output_file.cuda"
+echo ''
+
+./huffman_compression_cuda_o3.exe "$input_file" "$output_file.cuda"
 echo ''
 
 # Call huffman_compression_openmp with input and output filenames as arguments
 ./huffman_compression_openmp.exe "$input_file" "$output_file.omp"
-echo "Compression: $(echo "scale=2; $(stat -c '%s' $output_file.omp) / $(stat -c '%s' $input_file)" | bc -l)%"
+echo ''
+
+./huffman_compression_openmp_o2.exe "$input_file" "$output_file.omp"
+echo ''
+
+./huffman_compression_openmp_o3.exe "$input_file" "$output_file.omp"
 echo ''
 
 # Call huffman_compression_cuda_omp with input and output filenames as arguments
 ./huffman_compression_cuda_omp.exe "$input_file" "$output_file.cuda_omp"
-echo "Compression: $(echo "scale=2; $(stat -c '%s' $output_file.cuda_omp) / $(stat -c '%s' $input_file)" | bc -l)%"
+echo ''
+
+./huffman_compression_cuda_omp_o2.exe "$input_file" "$output_file.cuda_omp"
+echo ''
+
+./huffman_compression_cuda_omp_o3.exe "$input_file" "$output_file.cuda_omp"
 echo ''
